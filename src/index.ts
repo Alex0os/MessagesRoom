774ff9025a39fea1049b0433ad5268e4c3137323
Matias A. <>
2024-12-22 19:12:27 -0300

    feat: Make only '/', 'login' and 'signin' valid resources to get from the server
    - It doesn't render anything yet, it's just the logic to make
    those 3 resources valid for accessing the server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,44 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 import { readFile } from "fs";
 const app = express();
+
+// Let's make a callback to authenticate with JWT
+
+// TODO: Send form to make the user authenticate themselves and then be able to
+// access the website
+
+const VALIDURLS = "^(foo|bar|/)$";
+
+
+function isValidUrl(req: Request, res: Response, next: NextFunction): void {
+	// This isn't gonna be good for when I create chatrooms with the resource
+	// "/chatroom/{chatroom_id}, because the matches will become complicated,
+	// unless I decide to first verify that the url starts first with
+	// "/chatroom" resource
+	if (!req.url.match(VALIDURLS)) {
+		res.status(401).send("Resource doesn't exists");
+		return;
+	}
+
+	next();
+}
+
+
+function authorizeUser(req: Request, res: Response, next: NextFunction): void {
+	if (req.header("Authorization")) {
+		console.log("It has a token");
+		console.log(req.header("Authorization"));
+	}
+	else {
+		console.log("It doesn't have a token");
+		res.redirect(302, "/login")
+	}
+	next();
+}
+
+
+app.all(/^(?!\/signin$|\/login$|\/$).*/, isValidUrl, authorizeUser);
+
 
 app.get("/", (req, res) => {
 	new Promise<string>((resolve, reject) => {
@@ -16,6 +54,16 @@ app.get("/", (req, res) => {
 	}).
 		then(data => res.send(data)).
 		catch(err => res.send(err));
+})
+
+
+app.get("/login", (req, res) => {
+	res.send("This is the login resource");
+})
+
+
+app.get("/signin", (req, res) => {
+	res.send("This is the signin resource");
 })
 
 app.listen(8080);
